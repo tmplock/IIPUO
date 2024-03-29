a2c8930c2952822be429727e18a4646e9dc42258
firework <chrischrissmart@proton.me>
2024-03-29 18:30:15 +0900

    ICBuilder update ezugi

--- ICBuilder/processor.js
+++ ICBuilder/processor.js
@@ -58,7 +58,7 @@ exports.ProcessEzugi = async (listDB, listOverview, listOdds, listUpdateDB) => {
     if ( listDB.length <= 0 )
         return;
 
-    const res = await RDEzugi.GetRangeRD(listDB[0].createdAt, listDB[listDB.length-1].createdAt);
+    const res = await RDEzugi.GetRangeRD(listDB[0].createdAt, listDB[listDB.length-1].updatedAt);
     if (res == null)
     {
         // return;

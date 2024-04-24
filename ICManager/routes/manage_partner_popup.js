010958aea3e2b9ef3772dec02111058439d38ef9
firework <chrischrissmart@proton.me>
2024-04-25 01:23:05 +0900

    ICManager 유저 닉네임 수정시 버그 수정

--- ICManager/routes/manage_partner_popup.js
+++ ICManager/routes/manage_partner_popup.js
@@ -1069,7 +1069,7 @@ router.post('/request_agentinfo_modify',isLoggedIn, async (req, res) => {
         let listHistory = [];
         if ( user.strNickname != req.body.strNickname || user.strID != req.body.strID )
         {
-            let targetuser = await db.Users.findOne({where:{strNickname:req.body.strNickname}});
+            let targetuser = await db.Users.findOne({where:{strNickname:user.strNickname}});
             if ( null != targetuser )
             {
                 res.send({result:'ERROR', code:'UnknownUser'});

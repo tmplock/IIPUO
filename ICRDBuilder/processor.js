f197625eb504d5f3a9070874dad7d23d66f4c93a
ptarowana <124065059+ptarowana@users.noreply.github.com>
2024-05-21 14:05:48 +0900

    Update processor.js

--- ICRDBuilder/processor.js
+++ ICRDBuilder/processor.js
@@ -87,46 +87,77 @@ exports.ProcessCQ9 = async (listDB, listUpdateDB) => {
 }
 
 
+// exports.ProcessEzugi = async (listDB, listUpdateDB) => {
+
+//     if ( listDB.length <= 0 )
+//     {
+//         for (let i in listDB)
+//         {
+//             const cData = listDB[i];
+//             listUpdateDB.push({id:cData.id, strDetail:'', strResult:''});
+//         }
+//         return;
+//     }
+
+//     const res = await RDEzugi.GetRangeRD(listDB[0].createdAt, listDB[listDB.length-1].updatedAt);
+//     if (res == null)
+//     {
+//         for (let i in listDB)
+//         {
+//             const cData = listDB[i];
+//             listUpdateDB.push({id:cData.id, strDetail:'', strResult:''});
+//         }
+//         return;
+//     }
+
+//     for ( let i in listDB )
+//     {
+//         const cData = listDB[i];
+
+//         let listData = RDEzugi.GetRD(res, cData.strUniqueID);
+
+//         console.log(`##### ProcessEzugi listDB[${i}]`);
+//         console.log(listData);
+
+//         if (listData != null)
+//         {
+//             listUpdateDB.push({id:cData.id, strDetail:listData.strBets, strResult:listData.strCards});
+//         }
+//         else
+//         {
+//             listUpdateDB.push({id:cData.id, strDetail:'', strResult:''});
+//         }
+//     }    
+// }
+
 exports.ProcessEzugi = async (listDB, listUpdateDB) => {
 
-    if ( listDB.length <= 0 )
-    {
-        for (let i in listDB)
-        {
-            const cData = listDB[i];
-            listUpdateDB.push({id:cData.id, strDetail:'', strResult:''});
-        }
-        return;
-    }
+    // if ( listDB.length <= 0 )
+    // {
+    //     for (let i in listDB)
+    //     {
+    //         const cData = listDB[i];
+    //         listUpdateDB.push({id:cData.id, strDetail:'', strResult:''});
+    //     }
+    //     return;
+    // }
 
-    const res = await RDEzugi.GetRangeRD(listDB[0].createdAt, listDB[listDB.length-1].updatedAt);
-    if (res == null)
-    {
-        for (let i in listDB)
-        {
-            const cData = listDB[i];
-            listUpdateDB.push({id:cData.id, strDetail:'', strResult:''});
-        }
-        return;
-    }
+    // const res = await RDEzugi.GetRangeRD(listDB[0].createdAt, listDB[listDB.length-1].updatedAt);
+    // if (res == null)
+    // {
+    //     for (let i in listDB)
+    //     {
+    //         const cData = listDB[i];
+    //         listUpdateDB.push({id:cData.id, strDetail:'', strResult:''});
+    //     }
+    //     return;
+    // }
 
     for ( let i in listDB )
     {
         const cData = listDB[i];
 
-        let listData = RDEzugi.GetRD(res, cData.strUniqueID);
-
-        console.log(`##### ProcessEzugi listDB[${i}]`);
-        console.log(listData);
-
-        if (listData != null)
-        {
-            listUpdateDB.push({id:cData.id, strDetail:listData.strBets, strResult:listData.strCards});
-        }
-        else
-        {
-            listUpdateDB.push({id:cData.id, strDetail:'', strResult:''});
-        }
+        listUpdateDB.push({id:cData.id, strDetail:'', strResult:''});
     }    
 }
 

c9736823cb8ba4308b85797b975d5e153dcc1cf3
firework <chrischrissmart@proton.me>
2024-05-27 14:10:37 +0900

    ICManager 통장 암호화 처리, 반복 등록 기능

--- ICManager/utils/util.js
+++ ICManager/utils/util.js
@@ -22,3 +22,19 @@ exports.checkBlockNum = (id) => {
     }
     return false;
 }
+
+exports.replaceSpecialCharacters = (str) => {
+    let temp = str ?? '';
+    if (temp.length > 0) {
+        temp = temp.replace(/\\n/g, "\\n");
+        temp = temp.replace(/\\'/g, "\\'");
+        temp = temp.replace(/\\"/g, '\\"');
+        temp = temp.replace(/\\&/g, "\\&");
+        temp = temp.replace(/\\r/g, "\\r");
+        temp = temp.replace(/\\t/g, "\\t");
+        temp = temp.replace(/\\b/g, "\\b");
+        temp = temp.replace(/\\f/g, "\\f");
+        temp = temp.replace(/[\u0000-\u0019]+/g, "");
+    }
+    return temp;
+}

beef62f650424e3b7e37a6acf3fa3b55f295033b
ptarowana <124065059+ptarowana@users.noreply.github.com>
2024-05-28 16:36:18 +0900

    Update db.js

--- ICBuilder/db.js
+++ ICBuilder/db.js
@@ -1,24 +1,24 @@
 const Sequelize = require('sequelize');
 
+const sequelize = new Sequelize({
+    host:'db-mysql-sgp1-62759-do-user-11246819-0.c.db.ondigitalocean.com',
+    database:'iipc',
+    username:'doadmin',
+    password:'AVNS_FYsbSxfV0ADzDF4IiRJ',
+    dialect: 'mysql',
+    port:'25060',
+    timezone:'+09:00'
+});
 // const sequelize = new Sequelize({
-//     host: 'db-mysql-sgp1-27012-do-user-11246819-0.b.db.ondigitalocean.com',
-//     database: 'livecasino',
-//     username: 'iiplive',
-//     password: 'oLOHJkiQACPGuAgj',
+//     host: 'db-mysql-sgp1-78563-do-user-11246819-0.c.db.ondigitalocean.com',
+//     //database: 'iipcor',
+//     database:'iipc',
+//     username: 'doadmin',
+//     password: 'AVNS_M3_YxbEdNmi41c9HbLu',
 //     dialect: 'mysql',
 //     port:25060,
 //     timezone:'Asia/Seoul'
 // });
-const sequelize = new Sequelize({
-    host: 'db-mysql-sgp1-78563-do-user-11246819-0.c.db.ondigitalocean.com',
-    //database: 'iipcor',
-    database:'iipc',
-    username: 'doadmin',
-    password: 'AVNS_M3_YxbEdNmi41c9HbLu',
-    dialect: 'mysql',
-    port:25060,
-    timezone:'Asia/Seoul'
-});
 
 const db = {};
 db.Sequelize = Sequelize;

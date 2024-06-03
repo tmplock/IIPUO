ada80d53efdba74a346d9babb4d2eb168c895975
ptarowana <124065059+ptarowana@users.noreply.github.com>
2024-06-03 22:24:54 +0900

    123

--- ICUnover/app.js
+++ ICUnover/app.js
@@ -550,6 +550,8 @@ app.post('/request_checklogout', async (req, res) => {
 
     const objectResult = await IHelper.RequestAxios("http://165.22.102.70:3070/account/checklogout", {eType:'USER', strID:req.user.strID});
 
+    console.log(objectResult);
+
     if ( objectResult.result == 'OK' )
     {
         res.send({result:'OK', iLogout:objectResult.data.iLogout});

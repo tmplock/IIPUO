383eefc323d134d294d4d507a935ee059ba2b64b
ptarowana <124065059+ptarowana@users.noreply.github.com>
2024-06-03 21:16:25 +0900

    Update account.js

--- ICUnover/routes/account.js
+++ ICUnover/routes/account.js
@@ -57,7 +57,7 @@ router.get('/loginsuccess', async (req, res) => {
 
     console.log(`################################################## /account/loginsuccess`);
 
-    const objectResult = await IHelper.RequestAxios("http://165.22.102.70/account/login", {eType:'USER'});
+    const objectResult = await IHelper.RequestAxios("http://165.22.102.70:3070/account/login", {eType:'USER'});
 
     if ( objectResult.result == 'OK' )
     {

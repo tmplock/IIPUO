6c9161a6d0b3e28ea73086c261f85aa950ec40e4
firework <chrischrissmart@proton.me>
2024-06-10 15:49:17 +0900

    ICManager 설정 파일 수정(config, env.production)

--- ICManager/config/config.js
+++ ICManager/config/config.js
@@ -8,7 +8,7 @@ if(process.env.NODE_ENV === 'production') {
   console.log('production mode');
 } else {
   // 로컬개발시
-    require('dotenv').config({path: path.join(__dirname, '../.env.local')});
+  require('dotenv').config({path: path.join(__dirname, '../.env.local')});
   // 서버개발시
   // require('dotenv').config({ path: path.join(__dirname, '../.env.production') });
   console.log('development mode');

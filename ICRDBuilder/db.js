8d7e2874b2695b2979653fc8f585067a673a25fa
ptarowana <124065059+ptarowana@users.noreply.github.com>
2024-06-10 18:30:32 +0900

    123

--- ICRDBuilder/db.js
+++ ICRDBuilder/db.js
@@ -1,25 +1,26 @@
 const Sequelize = require('sequelize');
 
 //  Staging
-// const sequelize = new Sequelize({
-//     host:'db-mysql-sgp1-62759-do-user-11246819-0.c.db.ondigitalocean.com',
-//     database:'iipc',
-//     username:'doadmin',
-//     password:'AVNS_FYsbSxfV0ADzDF4IiRJ',
-//     dialect: 'mysql',
-//     port:'25060',
-//     timezone:'+09:00'
-// });
 const sequelize = new Sequelize({
-    host: 'db-mysql-sgp1-78563-do-user-11246819-0.c.db.ondigitalocean.com',
-    //database: 'iipcor',
+    host:'db-mysql-sgp1-62759-do-user-11246819-0.c.db.ondigitalocean.com',
     database:'iipc',
-    username: 'doadmin',
-    password: 'AVNS_M3_YxbEdNmi41c9HbLu',
+    username:'doadmin',
+    password:'AVNS_CHqkvP_p_JM6uWuOb73',
     dialect: 'mysql',
-    port:25060,
-    timezone:'Asia/Seoul'
+    port:'25060',
+    timezone:'+09:00'
 });
+//  Production
+// const sequelize = new Sequelize({
+//     host: 'db-mysql-sgp1-78563-do-user-11246819-0.c.db.ondigitalocean.com',
+//     //database: 'iipcor',
+//     database:'iipc',
+//     username: 'doadmin',
+//     password: 'AVNS_M3_YxbEdNmi41c9HbLu',
+//     dialect: 'mysql',
+//     port:25060,
+//     timezone:'Asia/Seoul'
+// });
 const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;

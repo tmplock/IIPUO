1b177a23dfd4ac29a22940119203ceeb6ada1e87
firework <chrischrissmart@proton.me>
2024-06-12 21:07:11 +0900

    ICManager 계좌관련 소스 머지

--- ICManager/config/config.js
+++ ICManager/config/config.js
@@ -4,14 +4,13 @@
 const path = require('path')
 
 if(process.env.NODE_ENV === 'production') {
-  // require('dotenv').config({ path: path.join(__dirname, '../.env.production') });
-  require('dotenv').config({path: path.join(__dirname, '../.env.local')});
+  require('dotenv').config({ path: path.join(__dirname, '../.env.production') });
   console.log('production mode');
 } else {
   // 로컬개발시
-  require('dotenv').config({path: path.join(__dirname, '../.env.local')});
+  // require('dotenv').config({path: path.join(__dirname, '../.env.local')});
   // 서버개발시
-  // require('dotenv').config({ path: path.join(__dirname, '../.env.production') });
+  require('dotenv').config({ path: path.join(__dirname, '../.env.production') });
   console.log('development mode');
   console.log(process.env.MYSQL_PASSWORD);
 }

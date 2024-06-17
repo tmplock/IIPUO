8efcc60e4c02b9ca85aac696ae27d64062e381b8
ptarowana <124065059+ptarowana@users.noreply.github.com>
2024-06-17 21:53:55 +0900

    Update app.js

--- ICBuilder/app.js
+++ ICBuilder/app.js
@@ -4,6 +4,9 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 
+const db = require('./db');
+db.sequelize.sync();
+
 const cron = require('./cron');
 
 const cPort = 3003;

460b85ace1f61f561b2dff015de08e8237ba8a4e
ptarowana <124065059+ptarowana@users.noreply.github.com>
2024-06-17 20:48:49 +0900

    123

--- ICUnover/config/config.js
+++ ICUnover/config/config.js
@@ -5,48 +5,50 @@
 const path = require('path')
 
 if(process.env.NODE_ENV === 'production') {
-    require('dotenv').config({ path: path.join(__dirname, '../.env.production') });
-    //require('dotenv').config({ path: path.join(__dirname, '../.env.production') });
-    console.log('production mode');
+    require('dotenv').config({ path: path.join(__dirname, '../.env.local') });
+      //require('dotenv').config({ path: path.join(__dirname, '../.env.production') });
+      //require('dotenv').config({ path: path.join(__dirname, '../.env.production') });
+      console.log('production mode');
 } else {
-  // 로컬개발시
-  require('dotenv').config({path: path.join(__dirname, '../.env.production')});
-  // 서버개발시
-  //require('dotenv').config({ path: path.join(__dirname, '../.env.production') });
-  console.log('development mode');
-  console.log(process.env.MYSQL_PASSWORD);
+    // 로컬개발시
+    require('dotenv').config({ path: path.join(__dirname, '../.env.local') });
+    //require('dotenv').config({path: path.join(__dirname, '../.env.production')});
+    // 서버개발시
+    //require('dotenv').config({ path: path.join(__dirname, '../.env.production') });
+    console.log('development mode');
+    console.log(process.env.MYSQL_PASSWORD);
 }
 
 const env = process.env;
 
 const development = {
-  username: env.MYSQL_USERNAME,
-  password: env.MYSQL_PASSWORD,
-  database: env.MYSQL_DATABASE,
-  host: env.MYSQL_HOST,
-  dialect: "mysql",
-  timezone:'+09:00',
-  port:env.MYSQL_PORT,
+    username: env.MYSQL_USERNAME,
+    password: env.MYSQL_PASSWORD,
+    database: env.MYSQL_DATABASE,
+    host: env.MYSQL_HOST,
+    dialect: "mysql",
+    timezone:'+09:00',
+    port:env.MYSQL_PORT,
 };
  
 const production = {
-  username: env.MYSQL_USERNAME,
-  password: env.MYSQL_PASSWORD,
-  database: env.MYSQL_DATABASE,
-  host: env.MYSQL_HOST,
-  dialect: "mysql",
-  timezone:'+09:00',
-  port:env.MYSQL_PORT,
+    username: env.MYSQL_USERNAME,
+    password: env.MYSQL_PASSWORD,
+    database: env.MYSQL_DATABASE,
+    host: env.MYSQL_HOST,
+    dialect: "mysql",
+    timezone:'+09:00',
+    port:env.MYSQL_PORT,
 };
  
 const test = {
-  username: env.MYSQL_USERNAME,
-  password: env.MYSQL_PASSWORD,
-  database: env.MYSQL_DATABASE_TEST,
-  host: env.MYSQL_HOST,
-  dialect: "mysql",
-  timezone:'+09:00',
-  port:env.MYSQL_PORT,
+    username: env.MYSQL_USERNAME,
+    password: env.MYSQL_PASSWORD,
+    database: env.MYSQL_DATABASE_TEST,
+    host: env.MYSQL_HOST,
+    dialect: "mysql",
+    timezone:'+09:00',
+    port:env.MYSQL_PORT,
 };
  
 module.exports = { development, production, test };

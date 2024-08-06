81373cc78c0fcbe869a883cc348535a2dcfbe547
ptarowana <124065059+ptarowana@users.noreply.github.com>
2024-08-06 16:53:14 +0900

    Update config.js

--- ICManager/config/config.js
+++ ICManager/config/config.js
@@ -3,18 +3,22 @@
 
 const path = require('path')
 
-if(process.env.NODE_ENV === 'production') {
-  require('dotenv').config({ path: path.join(__dirname, '../.env.production') });
-  console.log('production mode');
-} else {
-  // 로컬개발시
-  //require('dotenv').config({path: path.join(__dirname, '../.env.local')});
-  require('dotenv').config({ path: path.join(__dirname, '../.env.production') });
-  // 서버개발시
-  // require('dotenv').config({ path: path.join(__dirname, '../.env.production') });
-  console.log('development mode');
-  console.log(process.env.MYSQL_PASSWORD);
-}
+//require('dotenv').config({ path: path.join(__dirname, '../.env.production') });
+
+require('dotenv').config({path: path.join(__dirname, '../.env.local')});
+
+// if(process.env.NODE_ENV === 'production') {
+//   require('dotenv').config({ path: path.join(__dirname, '../.env.production') });
+//   console.log('production mode');
+// } else {
+//   // 로컬개발시
+//   //require('dotenv').config({path: path.join(__dirname, '../.env.local')});
+//   require('dotenv').config({ path: path.join(__dirname, '../.env.production') });
+//   // 서버개발시
+//   // require('dotenv').config({ path: path.join(__dirname, '../.env.production') });
+//   console.log('development mode');
+//   console.log(process.env.MYSQL_PASSWORD);
+// }
 
 const env = process.env;
 

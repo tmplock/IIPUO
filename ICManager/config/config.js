a68b8bebdc8a92948b52ea208fb5351b2147a3ca
ptarowanb <124065059+ptarowana@users.noreply.github.com>
2024-08-12 21:08:21 +0900

    Update config.js

--- ICManager/config/config.js
+++ ICManager/config/config.js
@@ -3,9 +3,9 @@
 
 const path = require('path')
 
-//require('dotenv').config({ path: path.join(__dirname, '../.env.production') });
+require('dotenv').config({ path: path.join(__dirname, '../.env.production') });
 
-require('dotenv').config({path: path.join(__dirname, '../.env.local')});
+//require('dotenv').config({path: path.join(__dirname, '../.env.local')});
 
 // if(process.env.NODE_ENV === 'production') {
 //   require('dotenv').config({ path: path.join(__dirname, '../.env.production') });
